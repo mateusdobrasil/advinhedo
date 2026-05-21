09ba5846199a2dc4f590542edde3b20836274093
Mateus Souza <mateus1984@gmail.com>
2026-05-21 15:50:58 -0300

    Update application with recent changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ibv.portal',
   appName: 'IBV APP',
-  webDir: '.next/standalone/public',
+  webDir: 'out',
   server: {
     androidScheme: 'https'
   },
